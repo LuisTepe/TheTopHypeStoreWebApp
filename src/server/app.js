cc44247faad908f6ff32e61f69e1308ce9abf0dc
Obregon <jesusarmando854@gmail.com>
2024-11-21 18:31:14 -0700

    Se implementó el user modal y las vistas de registro y login con su correcta funcionalidad

--- src/server/app.js
+++ src/server/app.js
@@ -1,9 +1,15 @@
 // src/server/app.js
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const db = require('./models'); // Importamos todos los modelos desde index.js
 
 const { Sequelize, DataTypes, Op } = require('sequelize'); // Asegúrate de importar Op
+
+// Importamos bcrypt y jsonwebtoken
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const SECRET_KEY = 'tu_clave_secreta_segura'; // Definmimos la clave secreta
 
 const app = express();
 
@@ -92,6 +98,96 @@ app.get('/api/productos/:id_producto/tallas', async (req, res) => {
     res.status(500).send('Error al obtener las tallas');
   }
 });
+
+        /*RUTAS PARA USUARIOS*/
+
+// Ruta para iniciar sesión
+app.post('/api/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ message: 'Por favor ingresa todos los campos.' });
+  }
+
+  try {
+    // Buscar al usuario por su email
+    const user = await db.usuarios.findOne({
+      where: { email },
+      // No es necesario especificar 'attributes' ya que ahora 'password' está incluido
+    });
+
+    if (!user) {
+      return res.status(400).json({ message: 'Credenciales inválidas.' });
+    }
+
+    // Comparar la contraseña ingresada con la almacenada en la base de datos
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: 'Credenciales inválidas.' });
+    }
+
+    // Generar el token JWT incluyendo el nombre
+    const token = jwt.sign(
+    { id_usuario: user.id_usuario, email: user.email, nombre: user.nombre },
+    SECRET_KEY,
+    { expiresIn: '1h' }
+    );
+
+    res.status(200).json({ message: 'Inicio de sesión exitoso', token });
+    } catch (error) {
+    console.error('Error al iniciar sesión:', error);
+    res.status(500).json({ message: 'Error al iniciar sesión' });
+  }
+});
+
+
+// Ruta para registrar usuario
+app.post('/api/register', async (req, res) => {
+  const { nombre, email, password } = req.body;
+
+  if (!nombre || !email || !password) {
+    return res.status(400).json({ message: 'Por favor ingresa todos los campos.' });
+  }
+
+  try {
+    const existingUser = await db.usuarios.findOne({ where: { email } });
+    if (existingUser) {
+      return res.status(400).json({ message: 'El correo ya está registrado.' });
+    }
+
+    // No hasheamos la contraseña aquí, el modelo se encargará de eso
+    const newUser = await db.usuarios.create({
+      nombre,
+      email,
+      password, // Usamos la contraseña directamente
+      roletype_id: 2, // Establecer el rol predeterminado como 2
+    });
+
+    res.status(201).json({ message: 'Usuario registrado con éxito', user: newUser });
+  } catch (error) {
+    console.error('Error al registrar usuario:', error);
+    res.status(500).json({ message: 'Error al registrar usuario' });
+  }
+});
+
+
+// Middleware de autenticación con JWT
+const authenticateJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (authHeader) {
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, SECRET_KEY, (err, user) => {
+      if (err) {
+        return res.status(403).json({ message: 'Token no válido' });
+      }
+      req.user = user;
+      next();
+    });
+  } else {
+    res.status(401).json({ message: 'Autorización requerida' });
+  }
+};
 
 // Iniciar el servidor
 app.listen(3000, () => {
